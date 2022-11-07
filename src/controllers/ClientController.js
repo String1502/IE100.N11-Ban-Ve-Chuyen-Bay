1d66e2529d0fdabdf30e12b90bf25b975c7c96ce
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2022-11-07 14:20:11 +0700

    Edit JS
    Làm lại JS cho tra cứu chuyến bay, gửi form qua Chọn chuyến bay thành công

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -4,22 +4,20 @@ const { QueryTypes } = require('sequelize');
 class ClientController {
     // "/"
     async index(req, res) {
-        let mangSanBay = [];
-        let Sanbay = [];
-
-        for (let i = 0; i < 10; i++) {
-            Sanbay.push({
-                masanbay: i.toString(),
-                tensanbay: 'Ten' + i,
-                matinhthanh: i.toString(),
-                tentinhthanh: 'TinhThanh' + i,
-            });
-            mangSanBay.push({ object: Sanbay[i], stringify: JSON.stringify(Sanbay[i]) });
-        }
+        let SanBays = [
+            { MaSanBay: 'TSN', TenSanBay: 'Tân Sơn Nhất', TinhThanh: 'HCM' },
+            { MaSanBay: 'DAD', TenSanBay: 'Haha', TinhThanh: 'Đà Nẵng' },
+        ];
+        let HangGhes = [{ MaHangGhe: 'Eco', TenHangGhe: 'Phổ thông' }];
+        let HanhKhach_Max = 7;
+        let ChuyenBay_Max = 5;
         try {
             return res.render('client/TraCuuChuyenBay', {
                 layout: 'client.handlebars',
-                SanBays: mangSanBay,
+                SanBays: SanBays,
+                HangGhes: HangGhes,
+                HanhKhach_Max: HanhKhach_Max,
+                ChuyenBay_Max: ChuyenBay_Max,
             });
         } catch (error) {
             console.log(error);
@@ -27,9 +25,16 @@ class ClientController {
     }
     // "/choose_flight"
     async choose_flight(req, res) {
+        let MangChuyenBay = JSON.parse(req.body.MangChuyenBay);
+        let HangGhe = JSON.parse(req.body.HangGhe);
+        let HanhKhach = JSON.parse(req.body.HanhKhach);
+        console.log(MangChuyenBay);
+        console.log(HangGhe);
+        console.log(HanhKhach);
         try {
             return res.render('client/ChonChuyenBay', {
                 layout: 'client.handlebars',
+                mangchuyenbay: MangChuyenBay,
             });
         } catch (error) {
             console.log(error);
--- src/public/javascript/client/ChonChuyenBay.js
+++ src/public/javascript/client/ChonChuyenBay.js
@@ -1,1 +1,41 @@
+const ThayDoiTimKiem = document.getElementById('ThayDoiTimKiem');
+const TomTat_Container = document.getElementById('TomTat_Container');
+const ChuyenBay_Container = document.getElementById('ChuyenBay_Container');
 
+// Bộ lọc
+const BoLoc_DiemDung_BayThang = document.getElementById('BoLoc_DiemDung_BayThang');
+const BoLoc_DiemDung_1DiemDung = document.getElementById('BoLoc_DiemDung_1DiemDung');
+const BoLoc_DiemDung_Hon2DiemDung = document.getElementById('BoLoc_DiemDung_Hon2DiemDung');
+const BoLoc_DiemDung_ThoiGianDung = document.getElementById('BoLoc_DiemDung_ThoiGianDung');
+const BoLoc_DiemDung_ThoiGianDung_Range = document.getElementById('BoLoc_DiemDung_ThoiGianDung_Range');
+
+const BoLoc_ThoiGianBay_CatCanh_SangSom = document.getElementById('BoLoc_ThoiGianBay_CatCanh_SangSom');
+const BoLoc_ThoiGianBay_CatCanh_Sang = document.getElementById('BoLoc_ThoiGianBay_CatCanh_Sang');
+const BoLoc_ThoiGianBay_CatCanh_Chieu = document.getElementById('BoLoc_ThoiGianBay_CatCanh_Chieu');
+const BoLoc_ThoiGianBay_CatCanh_Toi = document.getElementById('BoLoc_ThoiGianBay_CatCanh_Toi');
+
+const BoLoc_ThoiGianBay_HaCanh_SangSom = document.getElementById('BoLoc_ThoiGianBay_HaCanh_SangSom');
+const BoLoc_ThoiGianBay_HaCanh_Sang = document.getElementById('BoLoc_ThoiGianBay_HaCanh_Sang');
+const BoLoc_ThoiGianBay_HaCanh_Chieu = document.getElementById('BoLoc_ThoiGianBay_HaCanh_Chieu');
+const BoLoc_ThoiGianBay_HaCanh_Toi = document.getElementById('BoLoc_ThoiGianBay_HaCanh_Toi');
+
+const BoLoc_ThoiGianBay_ThoiGianBay = document.getElementById('BoLoc_ThoiGianBay_ThoiGianBay');
+const BoLoc_ThoiGianBay_ThoiGianBay_Range = document.getElementById('BoLoc_ThoiGianBay_ThoiGianBay_Range');
+
+const BoLoc_ThemBoLoc_GiaVe = document.getElementById('BoLoc_ThemBoLoc_GiaVe');
+const BoLoc_ThemBoLoc_GiaVe_Range = document.getElementById('BoLoc_ThemBoLoc_GiaVe_Range');
+
+const BoLoc_SapXep_GiaThapNhat = document.getElementById('BoLoc_SapXep_GiaThapNhat');
+const BoLoc_SapXep_CatCanhSomNhat = document.getElementById('BoLoc_SapXep_CatCanhSomNhat');
+const BoLoc_SapXep_CatCanhMuonNhat = document.getElementById('BoLoc_SapXep_CatCanhMuonNhat');
+const BoLoc_SapXep_HaCanhSomNhat = document.getElementById('BoLoc_SapXep_HaCanhSomNhat');
+const BoLoc_SapXep_HaCanhMuonNhat = document.getElementById('BoLoc_SapXep_HaCanhMuonNhat');
+const BoLoc_SapXep_ThoiGianBayNganNhat = document.getElementById('BoLoc_SapXep_ThoiGianBayNganNhat');
+
+const TomTat_Items = document.querySelectorAll('.TomTat_Item');
+
+for (let i = 0; i < TomTat_Items.length; i++) {
+    TomTat_Items[i].addEventListener('click', (e) => {
+        alert(e.target.querySelector('.TomTat_Item_Title_ThuTu').innerText);
+    });
+}

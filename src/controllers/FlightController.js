c0c65b5be65596ecdb75dc48148073ca9cd4734c
TriTran951 <thanhtritran951@gmail.com>
2022-11-11 16:53:15 +0700

    fix data sbtg

--- src/controllers/FlightController.js
+++ src/controllers/FlightController.js
@@ -59,9 +59,9 @@ let fullSearch = async (req, res) => {
 
 let search_flight = async (form_data) => {
     form_data.songuoi = 0;
-    for (let i = 0; i < form_data.hanhkhach.length; i++) {
-        form_data.songuoi = form_data.songuoi + parseInt(form_data.hanhkhach[i].value);
-    }
+    // for (let i = 0; i < form_data.hanhkhach.length; i++) {
+    //     form_data.songuoi = form_data.songuoi + parseInt(form_data.hanhkhach[i].value);
+    // }
     //tim tat ca chuyen bay hop le
     let list_ChuyenBaySuit = await db.sequelize.query(
         'select `MaChuyenBay`, `MaSanBayDi` as SanBayDi, `MaSanBayDen` as SanBayDen, `NgayGio` as ThoiGianDi, `ThoiGianBay` as ThoiGianBay, `GiaVeCoBan` as GiaVe from chuyenbay where DATE(NgayGio) = :ngaygio AND TrangThai = "ChuaKhoiHanh" AND  MaSanBayDi =  :sanbaydi  AND MaSanBayDen =  :sanbayden ',
@@ -231,7 +231,7 @@ let search_flight = async (form_data) => {
                     },
                     SanBayDen: list_ChuyenBaySuit[i].SanBayDen,
                     ThoiGianDen: list_ChuyenBaySuit[i].ThoiGianDen,
-                    ThoiGianDung_SanBayDen: 0,
+                    ThoiGianDung_SanBayDen: { Gio: 0, Phut: 0 },
                     ThoiGianBay: toHoursAndMinutes(thoigianbay),
                 };
                 chanbays.push(chanbay);
@@ -278,7 +278,7 @@ let search_flight = async (form_data) => {
 
                 //san bay trung gian -> san bay cuoi
                 thoigiandi = add_minutes(thoigianden, sbtg[j].ThoiGianDung);
-                thoigianbay = getMinDiff(thoigiandi, thoigianden);
+                thoigianbay = getMinDiff(thoigiandi, Date.parse(thoigianden_chuyenbay));
 
                 chanbay = {
                     SanBayDi: {
@@ -299,7 +299,7 @@ let search_flight = async (form_data) => {
                     },
                     SanBayDen: list_ChuyenBaySuit[i].SanBayDen,
                     ThoiGianDen: list_ChuyenBaySuit[i].ThoiGianDen,
-                    ThoiGianDung_SanBayDen: 0,
+                    ThoiGianDung_SanBayDen: { Gio: 0, Phut: 0 },
                     ThoiGianBay: toHoursAndMinutes(thoigianbay),
                 };
                 chanbays.push(chanbay);

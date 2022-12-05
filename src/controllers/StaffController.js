a581b537ba249aae817341c511dfeaba9fc88751
20520143 <92258237+20520143@users.noreply.github.com>
2022-12-05 11:31:16 +0700

    merge develop

--- src/controllers/StaffController.js
+++ src/controllers/StaffController.js
@@ -5,8 +5,26 @@ class StaffController {
     // "/staff/"
     async index(req, res) {
         try {
+            let SanBays = await db.sequelize.query(
+                'select MaSanBay , TenSanBay, TenTinhThanh as TinhThanh from sanbay, tinhthanh where sanbay.matinhthanh = tinhthanh.matinhthanh',
+                {
+                    type: QueryTypes.SELECT,
+                    raw: true,
+                },
+            );
+
+            let HangGhes = await db.sequelize.query(
+                `select MaHangGhe , TenHangGhe from hangghe where hangghe.TrangThai = 'apdung' `,
+                {
+                    type: QueryTypes.SELECT,
+                    raw: true,
+                },
+            );
+
             return res.render('staff/TraCuuChuyenBay', {
                 layout: 'staff.handlebars',
+                SanBays: SanBays,
+                HangGhes: HangGhes,
             });
         } catch (error) {
             console.log(error);
@@ -20,6 +38,38 @@ class StaffController {
             return res.render('staff/ChiTietChuyenBay', {
                 layout: 'staff.handlebars',
                 Package: Package,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    // "/staff/flightdetail/editdetail"
+    async editdetail(req, res) {
+        try {
+            let SanBays = await db.sequelize.query(
+                'select MaSanBay , TenSanBay, TenTinhThanh as TinhThanh from sanbay, tinhthanh where sanbay.matinhthanh = tinhthanh.matinhthanh',
+                {
+                    type: QueryTypes.SELECT,
+                    raw: true,
+                },
+            );
+
+            let HangGhes = await db.sequelize.query(
+                `select MaHangGhe , TenHangGhe from hangghe where hangghe.TrangThai = 'apdung' `,
+                {
+                    type: QueryTypes.SELECT,
+                    raw: true,
+                },
+            );
+
+            let Flight_Edit = JSON.parse(req.body.Flight_Edit);
+            Flight_Edit['SanBays'] = structuredClone(SanBays);
+            Flight_Edit['HangGhes'] = structuredClone(HangGhes);
+
+            return res.render('staff/ChinhSuaChuyenBay', {
+                layout: 'staff.handlebars',
+                Flight_Edit: Flight_Edit,
+                Flight_EditJS: JSON.stringify(Flight_Edit),
             });
         } catch (error) {
             console.log(error);
@@ -291,4 +341,5 @@ class StaffController {
         }
     }
 }
+
 module.exports = new StaffController();

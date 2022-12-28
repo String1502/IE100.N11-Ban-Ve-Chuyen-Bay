48502737b2e825a77768ff8bf3bc5798650717c6
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2022-12-28 18:48:59 +0700

    Thêm User vào hóa đơn

--- src/controllers/HinhThucThanhToanController.js
+++ src/controllers/HinhThucThanhToanController.js
@@ -120,6 +120,7 @@ class HinhThucThanhToanController {
         var MaHoaDonHienThi = vnp_Params['vnp_TxnRef'].toString();
         // [MaUser]-[MaHoaDon]-[MaHangVe] // nếu MaUser = null thì MaUser = GUEST
         var MaHoaDon = MaHoaDonHienThi.split('-')[1];
+        var MaUser = MaHoaDonHienThi.split('-')[0];
 
         if (secureHash === signed) {
             var rspCode = vnp_Params['vnp_ResponseCode'].toString();
@@ -129,7 +130,7 @@ class HinhThucThanhToanController {
                 var NgayGioThanhToan = new Date();
 
                 // Thanh toán và gửi vé điện tử
-                let ThanhToan = await HoaDonController.ThanhToan(MaHoaDon, MaHTTT, NgayGioThanhToan);
+                let ThanhToan = await HoaDonController.ThanhToan(MaHoaDon, MaHTTT, NgayGioThanhToan, MaUser);
 
                 if (ThanhToan == 1) {
                     KetQuaThanhToan.ThanhCong = true;
